20e995b6b49302487e226ca3a6fa17e6cc2bc7d5
CDAC BANGALORE <cdacbangalore404@gmail.com>
2023-03-08 10:39:01 +0530

    Implement create post api to create custom post to store in the database

--- models/Post.php
+++ models/Post.php
@@ -31,5 +31,31 @@ class Post {
 
         return $stmt;
     }
+	// create posts 
+    public function create_post() {
+        $sql = "
+            INSERT INTO ".$this->table." SET title = :title, body = :body, author = :author, category_id = :category_id
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+
+        // clean data
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        // bind parameter
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":body", $this->body);
+        $stmt->bindParam(":author", $this->author);
+        $stmt->bindParam(":category_id", $this->category_id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        printf("Error: %s", $stmt->error);
+        return false;
+    }
 }
 ?>
